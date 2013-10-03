1d90fa8763657d8a622e2004a0842b8a9c7ab354
Daniel Falster <daniel.falster@mq.edu.au>
2013-10-04 08:29:57 +1000

    Update dependency graph

--- makeDependencyGraph.R
+++ makeDependencyGraph.R
@@ -1,10 +1,8 @@
 rm(list=ls())
-library("devtools")
-install_github("dataMashR","dfalster")
 
+#library("devtools")
+#install_github("dataMashR","dfalster")
 #install_github("rDependencyGraph","dfalster")
 #library(rDependencyGraph)
 
-source("~/git/rDependencyGraph/R/functions.R")
-
-plotDependencyForAPackage("dataMashR")
+dependencyForAPackage("dataMashR")
