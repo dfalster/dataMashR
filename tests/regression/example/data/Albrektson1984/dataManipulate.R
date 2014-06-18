f0605c20124070aec24fcfae8a1609c2be237dcb
Daniel Falster <daniel.falster@mq.edu.au>
2014-06-18 14:26:43 +1000

    Update example data

--- tests/regression/example/data/Albrektson1984/dataManipulate.R
+++ tests/regression/example/data/Albrektson1984/dataManipulate.R
@@ -1,16 +1,17 @@
 manipulate <- function(raw) {
-
-
+  
+  
   raw$grouping <- makeGroups(raw, "fertilized")
-
+  
   # remove crown surface area, was calculated from crown width and length
   raw$a.cs <- NULL
-
+  
   # h.c was defined as crown length, should be height to base of crown
   raw$h.c <- with(raw, h.t - h.c)
-
+  
   # zero m.rt indicates it was not measured
   raw$m.rt[raw$m.rt == 0] <- NA
-
+  
   raw
 }
+
--- tests/regression/example/data/Burger1953/dataManipulate.R
+++ tests/regression/example/data/Burger1953/dataManipulate.R
@@ -1,6 +1,7 @@
 manipulate <- function(raw) {
-
+    
   raw$grouping <- makeGroups(raw, c("site","altitude"))
-
+  
   raw
 }
+
